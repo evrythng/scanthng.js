b0172ef4b85dda174a4f7590ad7aaba1bb1904eb
C-D-Lewis <Bonsitm@gmail.com>
2020-12-09 15:55:09 +0000

    Update playground app for Application or Operator

--- test/playground/index.js
+++ test/playground/index.js
@@ -1,86 +1,123 @@
 evrythng.use(ScanThng);
 
+/**
+ * Get an element by ID.
+ * 
+ * @param {string} id - ID to find.
+ * @returns {HTMLElement}
+ */
+const get = id => document.getElementById(id);
+
+/** UI component handles */
 const UI = {
-  buttonBase64DataScan: document.getElementById('input-base64data-scan'),
-  buttonIdentify: document.getElementById('input-identify'),
-  buttonScan: document.getElementById('input-scan'),
-  buttonScanStream: document.getElementById('input-scanstream'),
-  buttonStopStream: document.getElementById('input-stopstream'),
-  buttonScanCode: document.getElementById('input-scancode'),
-  buttonStopScanCode: document.getElementById('input-stopscancode'),
-  buttonUseKey: document.getElementById('input-use-api-key'),
-  inputApiKey: document.getElementById('input-app-api-key'),
-  inputIdentifyType: document.getElementById('input-identify-type'),
-  inputIdentifyValue: document.getElementById('input-identify-value'),
-  inputScanBase64Data: document.getElementById('input-scan-base64data-data'),
-  inputScanMethod: document.getElementById('input-scan-method'),
-  inputScanType: document.getElementById('input-scan-type'),
-  inputScanstreamMethod: document.getElementById('input-scanstream-method'),
-  inputScanstreamType: document.getElementById('input-scanstream-type'),
-  inputScanstreamOffline: document.getElementById('input-scanstream-offline'),
-  inputScanstreamAutostop: document.getElementById('input-scanstream-autostop'),
+  buttonBase64DataScan: get('input-base64data-scan'),
+  buttonIdentify: get('input-identify'),
+  buttonScan: get('input-scan'),
+  buttonScanStream: get('input-scanstream'),
+  buttonStopStream: get('input-stopstream'),
+  buttonScanCode: get('input-scancode'),
+  buttonStopScanCode: get('input-stopscancode'),
+  buttonUseKey: get('input-use-api-key'),
+  inputApiKey: get('input-app-api-key'),
+  inputIdentifyType: get('input-identify-type'),
+  inputIdentifyValue: get('input-identify-value'),
+  inputScanBase64Data: get('input-scan-base64data-data'),
+  inputScanMethod: get('input-scan-method'),
+  inputScanType: get('input-scan-type'),
+  inputScanstreamMethod: get('input-scanstream-method'),
+  inputScanstreamType: get('input-scanstream-type'),
+  inputScanstreamOffline: get('input-scanstream-offline'),
+  inputScanstreamAutostop: get('input-scanstream-autostop'),
 };
 
+/** Container ID for scanStream */
 const SCANSTREAM_CONTAINER_ID = 'scanstream-container';
+/** Container ID for scanQrCode */
 const SCANCODE_CONTAINER_ID = 'scancode-container';
 
+/**
+ * Get a query param value.
+ *
+ * @param {string} key - Key to use.
+ * @returns {string} Value, if found.
+ */
 const getQueryParam = key => new URLSearchParams(window.location.search).get(key);
 
-const loadApp = () => {
-  const apiKey = getQueryParam('app');
-  if (apiKey) {
-    UI.inputApiKey.value = apiKey || '';
-    window.app = new evrythng.Application(apiKey);
-    window.app.init().catch(e => alert('Invalid Application API Key'));
-  }
+/**
+ * Load Application scope.
+ */
+const loadScope = () => {
+  const appApiKey = getQueryParam('app');
+  const operatorApiKey = getQueryParam('operator');
+  if (!appApiKey && !operatorApiKey) return;
+
+  UI.inputApiKey.value = appApiKey || operatorApiKey || '';
+  window.scope = appApiKey
+    ? new evrythng.Application(appApiKey)
+    : new evrythng.Operator(operatorApiKey);
+  window.scope.init().catch(() => alert('API key is invalid'));
 };
 
+/**
+ * Load filter from query params
+ */
 const loadFilter = () => {
-  const method = getQueryParam('method') || '2d';
-  const type = getQueryParam('type') || 'qr_code';
-
-  UI.inputScanMethod.value = method;
-  UI.inputScanType.value = type;
+  UI.inputScanMethod.value = getQueryParam('method') || '2d';
+  UI.inputScanType.value = getQueryParam('type') || 'qr_code';
 };
 
+/**
+ * Handle results.
+ *
+ * @param {*} res 
+ */
 const handleResults = (res) => {
+  console.log(JSON.stringify(res, null, 2));
+
   if (typeof res === 'string') {
-    console.log(res);
     alert(res);
     return;
   }
 
-  console.log(JSON.stringify(res, null, 2));
   const numResults = res.length;
   let numFound = 0;
   if (numResults) {
     numFound = res[0].results.length;
   }
 
-  alert(`numResults: ${numResults}, numFound: ${numFound}`);
+  alert(`Results: ${numResults}, Found: ${numFound}`);
 };
 
+/**
+ * Test a function and handle results and errors.
+ *
+ * @param {Function} f - Function to test.
+ */
 const testFunction = f => f().catch(console.log).then(handleResults);
 
+/**
+ * When the page loads.
+ */
 const onLoad = () => {
-  loadApp();
+  loadScope();
   loadFilter();
 
+  // Add button click listeners
   UI.buttonUseKey.addEventListener('click', () => {
-    window.app = new evrythng.Application(UI.inputApiKey.value);
-    window.app.init().catch(e => alert('Invalid Application API Key'));
+    window.scope = new evrythng.Application(UI.inputApiKey.value);
+    window.scope.init().catch(() => alert('Invalid Application API Key'));
   });
 
   UI.buttonIdentify.addEventListener('click', () => {
     const type = UI.inputIdentifyType.value;
     const { value } = UI.inputIdentifyValue;
-    testFunction(() => window.app.identify({ filter: { type, value } }));
+    testFunction(() => window.scope.identify({ filter: { type, value } }));
   });
 
   UI.buttonScan.addEventListener('click', () => {
     const method = UI.inputScanMethod.value;
     const type = UI.inputScanType.value;
-    testFunction(() => window.app.scan({
+    testFunction(() => window.scope.scan({
       filter: { method, type },
       imageConversion: {
         exportFormat: 'image/jpeg',
@@ -92,7 +129,7 @@ const onLoad = () => {
     const method = UI.inputScanMethod.value;
     const type = UI.inputScanType.value;
     const base64Data = UI.inputScanBase64Data.value;
-    testFunction(() => window.app.scan(base64Data, {
+    testFunction(() => window.scope.scan(base64Data, {
       filter: { method, type },
       imageConversion: {
         exportFormat: 'image/jpeg',
@@ -111,11 +148,11 @@ const onLoad = () => {
       offline,
       autoStop,
     };
-    testFunction(() => window.app.scanStream(opts));
+    testFunction(() => window.scope.scanStream(opts));
   });
 
   UI.buttonStopStream.addEventListener('click', () => {
-    window.app.stopStream();
+    window.scope.stopStream();
   });
 
   UI.buttonScanCode.addEventListener('click', () => {
