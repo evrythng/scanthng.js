086a3982e2f872a5c0eba4ab9397724a337e1171
Kevin Chan <k3vink3vin@users.noreply.github.com>
2023-02-10 14:12:11 +0000

    Fix lint

--- src/stream.js
+++ src/stream.js
@@ -429,7 +429,8 @@ const scanCode = (opts, scope) => {
     throw new Error('Please specify \'containerId\' where the video element can be added as a child');
   }
 
-  // Begin the stream by selecting a read facing camera, the last usually being the HQ sensor on Android devices
+  // Begin the stream by selecting a read facing camera, the last usually being the HQ sensor on
+  // Android devices
   return navigator.mediaDevices.enumerateDevices()
     .then((devices) => devices.filter((device) => device.kind === 'videoinput'))
     .then((devices) => navigator.mediaDevices.getUserMedia({
